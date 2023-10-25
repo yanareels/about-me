cdd794fbf1478aae291d8d4a7d59bb0b4d030578
yanareels <147980255+yanareels@users.noreply.github.com>
2023-10-25 09:42:46 +0300

    Update more-2.js
    add js

--- more-2.js
+++ more-2.js
@@ -1,16 +1,46 @@
-// читать далее
-function myFunctionTwo() {
-    let dotsTwo = document.getElementById("dotsTwo");
-    let moreTextTwo = document.getElementById("moreTwo");
-    let btnTextTwo = document.getElementById("myBtnTwo");
-  
-    if (dotsTwo.style.display === "none") {
-      dotsTwo.style.display = "inline";
-      btnTextTwo.innerHTML = "Читать больше"; 
-      moreTextTwo.style.display = "none";
-    } else {
-      dotsTwo.style.display = "none";
-      btnTextTwo.innerHTML = "Скрыть текст"; 
-      moreTextTwo.style.display = "inline";
-    }
-  }
+function toggleText() {
+ 
+            // Get all the elements from the page
+            let points =
+                document.getElementById("points");
+ 
+            let showMoreText =
+                document.getElementById("moreText");
+ 
+            let buttonText =
+                document.getElementById("textButton");
+ 
+            // If the display property of the dots 
+            // to be displayed is already set to 
+            // 'none' (that is hidden) then this 
+            // section of code triggers
+            if (points.style.display === "none") {
+ 
+                // Hide the text between the span
+                // elements
+                showMoreText.style.display = "none";
+ 
+                // Show the dots after the text
+                points.style.display = "inline";
+ 
+                // Change the text on button to 
+                // 'Show More'
+                buttonText.innerHTML = "Show More";
+            }
+ 
+            // If the hidden portion is revealed,
+            // we will change it back to be hidden
+            else {
+ 
+                // Show the text between the
+                // span elements
+                showMoreText.style.display = "inline";
+ 
+                // Hide the dots after the text
+                points.style.display = "none";
+ 
+                // Change the text on button
+                // to 'Show Less'
+                buttonText.innerHTML = "Show Less";
+            }
+        }
